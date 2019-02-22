226f2f02279ec37e0f572fc32335f4366a202ba0
RayTsu <38807829+RayTsu@users.noreply.github.com>
2019-02-22 13:36:28 +0800

    更改在本地机器执行为到远程机器执行

--- ignore_input_pass.sh
+++ ignore_input_pass.sh
@@ -2,10 +2,9 @@
 set timeout 100
 set password "123456!$"
 set ip [lindex $argv 0]
-spawn ssh xl@$ip -o StrictHostKeyChecking=no 
+spawn ssh xl@$ip -o StrictHostKeyChecking=no ls -al /usr/local && ls -al ~/.
 expect "xl@$ip's password:"
 send "$password\n"
 sleep 1
-spawn systemctl status zabbix-agent
 interact
 exit
