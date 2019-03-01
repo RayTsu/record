97aff97aa604560fcb6979bdd2357e390289b29a
RayTsu <38807829+RayTsu@users.noreply.github.com>
2019-03-01 10:30:35 +0800

    spawn添加双引号执行命令或脚本

--- ignore_input_pass.sh
+++ ignore_input_pass.sh
@@ -2,7 +2,8 @@
 set timeout 100
 set password "123456!$"
 set ip [lindex $argv 0]
-spawn ssh xl@$ip -o StrictHostKeyChecking=no ls -al /usr/local && ls -al ~/.
+#spawn ssh xl@$ip -o StrictHostKeyChecking=no ls -al /usr/local && ls -al ~/.
+spawn ssh xl@$ip "sudo chown test.test /app/aa -R;echo $$"
 expect "xl@$ip's password:"
 send "$password\n"
 sleep 1
